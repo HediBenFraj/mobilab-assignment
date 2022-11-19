41107b65a83eaae80bd90054853692118de0e6d5
Hedi <hedi.benfraj@bareos.com>
2022-11-19 18:31:56 +0100

    tweaked routes that apply authentication middleware to all for the moment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BankAccountModule } from './bank-account/bank-account.module';
 import { config } from './config/keys';
+import { AuthenticationMiddleware } from './middleware/authentication.middleware';
+import { RequestService } from './request.service';
 import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
   imports: [BankAccountModule, TransactionModule, MongooseModule.forRoot(config.mongoURI)],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,AuthenticationMiddleware,RequestService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthenticationMiddleware).forRoutes('*')
+  }
+}
--- src/bank-account/bank-account.module.ts
+++ src/bank-account/bank-account.module.ts
@@ -1,18 +1,14 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { BankAccountService } from './bank-account.service';
 import { BankAccountController } from './bank-account.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BankAccountSchema } from './schemas/bank-account.schema';
 import { RequestService } from 'src/request.service';
-import { AuthenticationMiddleware } from 'src/middleware/authentication.middleware';
+
 
 @Module({
   imports: [MongooseModule.forFeature([{name: 'BankAccount',schema:BankAccountSchema}])],
   controllers: [BankAccountController],
   providers: [BankAccountService, RequestService]
 })
-export class BankAccountModule implements NestModule{
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthenticationMiddleware).forRoutes('*')
-  }
-}
+export class BankAccountModule {}
