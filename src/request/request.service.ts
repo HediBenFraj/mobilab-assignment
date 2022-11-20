530c81638a3a822d5cb3f8a8b82ad97ce832694b
Hedi <hedi.benfraj@bareos.com>
2022-11-20 23:13:29 +0100

    finished dockerizing app

--- src/request/request.service.ts
+++ src/request/request.service.ts
@@ -5,12 +5,10 @@ export class RequestService {
     private userId: string
 
     setUserId(userId : string){
-        console.log("setting")
         this.userId = userId
     }
 
     getUserId(){
-        console.log("getting")
         return this.userId
     }
 }
